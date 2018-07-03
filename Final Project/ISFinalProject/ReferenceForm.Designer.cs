d016cf788a377c30b8e69198dbff63f6327ad6dd
jinseisan <781972452@QQ.com>
2018-07-03 09:17:45 +0800

    2018.7.3

--- Final Project/ISFinalProject/ReferenceForm.Designer.cs
+++ Final Project/ISFinalProject/ReferenceForm.Designer.cs
@@ -36,6 +36,7 @@
             this.outputTextBox = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,7 +44,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("宋体", 15F);
-            this.label1.Location = new System.Drawing.Point(50, 42);
+            this.label1.Location = new System.Drawing.Point(18, 38);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(99, 20);
             this.label1.TabIndex = 0;
@@ -52,17 +53,17 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("宋体", 12F);
-            this.textBox1.Location = new System.Drawing.Point(237, 36);
+            this.textBox1.Location = new System.Drawing.Point(127, 36);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(285, 26);
+            this.textBox1.Size = new System.Drawing.Size(436, 26);
             this.textBox1.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("宋体", 15F);
-            this.label2.Location = new System.Drawing.Point(50, 88);
+            this.label2.Location = new System.Drawing.Point(18, 91);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(99, 20);
             this.label2.TabIndex = 2;
@@ -71,17 +72,17 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(54, 123);
+            this.dataGridView1.Location = new System.Drawing.Point(22, 114);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(596, 255);
+            this.dataGridView1.Size = new System.Drawing.Size(650, 255);
             this.dataGridView1.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("宋体", 12F);
-            this.label3.Location = new System.Drawing.Point(50, 398);
+            this.label3.Location = new System.Drawing.Point(19, 383);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(128, 16);
             this.label3.TabIndex = 4;
@@ -90,40 +91,51 @@
             // outputTextBox
             // 
             this.outputTextBox.Font = new System.Drawing.Font("宋体", 10F);
-            this.outputTextBox.Location = new System.Drawing.Point(54, 417);
+            this.outputTextBox.Location = new System.Drawing.Point(22, 412);
             this.outputTextBox.Multiline = true;
             this.outputTextBox.Name = "outputTextBox";
             this.outputTextBox.ReadOnly = true;
             this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.outputTextBox.Size = new System.Drawing.Size(597, 79);
+            this.outputTextBox.Size = new System.Drawing.Size(650, 79);
             this.outputTextBox.TabIndex = 5;
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("宋体", 10F);
-            this.textBox2.Location = new System.Drawing.Point(54, 542);
+            this.textBox2.Location = new System.Drawing.Point(22, 522);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
             this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox2.Size = new System.Drawing.Size(597, 84);
+            this.textBox2.Size = new System.Drawing.Size(650, 84);
             this.textBox2.TabIndex = 6;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("宋体", 12F);
-            this.label4.Location = new System.Drawing.Point(51, 509);
+            this.label4.Location = new System.Drawing.Point(19, 494);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(160, 16);
             this.label4.TabIndex = 7;
             this.label4.Text = "引文文献提取关键词:";
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 12F);
+            this.button1.Location = new System.Drawing.Point(569, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 26);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "提取关键词";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // ReferenceForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(707, 638);
+            this.ClientSize = new System.Drawing.Size(692, 662);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.outputTextBox);
@@ -132,6 +144,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
             this.Name = "ReferenceForm";
             this.Text = "引文文献信息";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -150,5 +163,6 @@
         private System.Windows.Forms.TextBox outputTextBox;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
     }
 }
